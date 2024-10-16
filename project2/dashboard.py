4cfdb934db6d6ee09e65e1beb647868e649d2d37
ciardileo <leolopes.ciardi04@gmail.com>
2024-10-16 19:52:06 -0300

    started my second project using streamlit

---
+++ project2/dashboard.py
@@ -0,0 +1,38 @@
+"""
+Objetivo: apresentar os dados sobre a economia dos países em dashboards
+- Deve ser possível ver os dados mundiais por ano
+- Deve ser possível ver os dados por paíshttp://localhost:8501/
+"""
+
+# imports
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+# configurações da página
+st.set_page_config(layout="wide")
+
+# dataframe
+df = pd.read_csv("project2/global_income_inequality.csv")
+
+# organização dos dados
+df["Year"] = pd.to_datetime(df["Year"], format="%Y").dt.year
+df = df.sort_values("Year")
+
+# dashboard
+year = st.sidebar.selectbox("Ano", df["Year"].unique())
+
+df_filter = df[df["Year"] == year]
+df_filter = df_filter.sort_values("Country")
+df_filter[["Country", "Population", "Average Income (USD)", "Income Group", "Gini Index", "Top 10% Income Share (%)"]]
+
+
+# caixas
+col1 = st.columns(1)[0]
+
+# gráficos 
+
+# gráfico de renda média anual
+df_mean = df.groupby("Year")["Average Income (USD)"].mean().reset_index()
+fig_year = px.bar(df_mean, x="Year", y="Average Income (USD)", title="Renda média por ano")
+col1.plotly_chart(fig_year)
